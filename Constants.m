66b13dd9f27bb2fc3c759a01706a059c649fe480
tclos <93877648+tclos@users.noreply.github.com>
2025-08-08 17:10:44 -0300

    Add files via upload

---
+++ Constants.m
@@ -0,0 +1,6 @@
+classdef Constants
+    properties( Constant = true )
+      G = 6.67430e-11;              % Gravitational constant [m^3/kg/s^2]
+      M_sun = 1.989e30;             % Mass of the Sun [kg]
+    end
+ end
